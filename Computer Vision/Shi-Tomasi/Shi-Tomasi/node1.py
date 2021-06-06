655df8491e68b387fb0ea4cb3181cf8b0aa4862a
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2021-06-06 18:37:47 +0200

    cv shi-tomasi lab

--- Computer Vision/Shi-Tomasi/Shi-Tomasi/node1.py
+++ Computer Vision/Shi-Tomasi/Shi-Tomasi/node1.py
@@ -19,6 +19,7 @@ from matplotlib import pyplot as plt
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import timeit
 
 class my_node(Node):
     def __init__(self):
@@ -29,22 +30,19 @@ class my_node(Node):
         
 
     
-    #def show(self):
-        #gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        #corners = cv.goodFeaturesToTrack(gray,25,0.01,10)
-        #corners = np.int0(corners)
-        #for i in corners:
-        #    x,y = i.ravel()
-        #    cv.circle(img,(x,y),3,255,-1)
-            
 
     def sub_call(self,msg):
         self.bridge = CvBridge()
         black = np.zeros([240, 320, 3])
         self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         gray = cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
+        start = timeit.default_timer()
+
         corners = cv.goodFeaturesToTrack(gray,25,0.01,10)
         corners = np.int0(corners)
+        stop = timeit.default_timer()
+        self.get_logger().info(str(stop - start))
+
         for i in corners:
             x,y = i.ravel()
             cv.circle(black,(x,y),3,255,-1)
